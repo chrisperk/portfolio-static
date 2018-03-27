3d4c3d08eda85108045a637fbd4edec8c6ba0a11
chrisperk <christopherperkins.512@gmail.com>
2018-03-26 20:26:49 -0500

    added grayscale filter that colors on hover

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const anchors = document.querySelectorAll('.anchor');
+const workCards = document.querySelectorAll('.work-card');
 
 function scrollToAnchor(e) {
     const scrollSpot = document.querySelector(`#${e.target.name}`).offsetTop;
@@ -8,6 +9,24 @@ function scrollToAnchor(e) {
     });
 }
 
+function colorPicture() {
+    console.log(this, 'mouse in');
+    this.querySelector('.card-pic').classList.add('color');
+}
+
+function fadePicture() {
+    console.log(this, 'mouse out');    
+    this.querySelector('.card-pic').classList.remove('color');  
+}
+
 anchors.forEach(anchor => {
     anchor.addEventListener('click', e => scrollToAnchor(e));
+});
+
+workCards.forEach(card => {
+    card.addEventListener('mouseover', colorPicture);
+});
+
+workCards.forEach(card => {
+    card.addEventListener('mouseout', fadePicture);
 });
